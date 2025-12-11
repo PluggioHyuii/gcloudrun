e87ac8785c879868d5cb6c1f7915ad0578114e57
PluggioHyuii <changtong.startup@gmail.com>
2025-12-11 13:28:17 +0700

    dburi

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', async (req, res) => {
   let port = process.env.PORT || 'not defined';
+  let db_uri = process.env.DB_URI || 'not defined';
   let views = 'index';
-  let context = { title: 'Pluggio hello', port: port };
+  let context = { 
+    title: 'Pluggio hello', 
+    port: port,
+    db_uri: db_uri
+  };
   res.render(views, context);
 });
 
